41106562f00f1a0d4455211cc61cfbbec9b4a135
kaleem molani <abdul.kaleem@girikon.com>
2022-01-05 13:48:09 +0530

    naming variables

--- index.js
+++ index.js
@@ -2,25 +2,22 @@ const { default: axios } = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
 const app = express();
-var arrayOfThisRow = {};
-var list 
+var moveJson = {};
+var moveList 
 
 const port = process.env.PORT || 3000;
 axios.get('https://www.chessgames.com/chessecohelp.html')
       .then(response => {
           const $ = cheerio.load(response.data);
-          list = response.data;
-
+          moveList = response.data;
           $("table").each(function() {
               var tableData = $(this).find('td');
               if (tableData.length > 0) {
                   tableData.each(function(index,el) {
                     if(index%2===0){
-                      arrayOfThisRow[$(this).text()] = $(this).next().find('font').find('font').text();
-                      
+                      moveJson[$(this).text()] = $(this).next().find('font').find('font').text();
                     }
                   });
-                  
               }
             });
       }).catch(error => {
@@ -28,37 +25,37 @@ axios.get('https://www.chessgames.com/chessecohelp.html')
       });
 
 app.get('/', (req, res) => {
-  res.send(list);
-
+  if(moveList!==undefined){
+  res.send(moveList);
+  }else{
+    res.send("data is not loading yet, please wait");
+  }
 });
 app.get('/:code/*',(req,res)=>{
-  let s= arrayOfThisRow[req.params.code].toString();
-  let sindex = s.toLowerCase().search(req.params["0"].split("/").pop())
-  console.log(typeof s,s,sindex,req.params["0"].split("/").pop().toLowerCase())
-  let ns = s.substring(sindex);
-  console.log(ns.split(" ")[1])
-  if(sindex===-1){
+  let moveRelatedToCode= moveJson[req.params.code].toString();
+  let lastMoveIndex = moveRelatedToCode.toLowerCase().search(req.params["0"].split("/").pop())
+  console.log(typeof moveRelatedToCode,moveRelatedToCode,lastMoveIndex,req.params["0"].split("/").pop().toLowerCase())
+  let moveListAfterLastMove = moveRelatedToCode.substring(lastMoveIndex);
+  if(lastMoveIndex===-1){
     res.send("Move Not Found")
   }else{
-    if(ns.split(" ")[1].length>1){
-    console.log(ns.split(" ")[1].length)
-    res.send(ns.split(" ")[1]);
+    if(moveListAfterLastMove.split(" ")[1].length>1){
+    console.log(moveListAfterLastMove.split(" ")[1].length)
+    res.send(moveListAfterLastMove.split(" ")[1]);
     }
-    if(ns.split(" ")[1].length==1){
-    res.send(ns.split(" ")[2]);
+    if(moveListAfterLastMove.split(" ")[1].length==1){
+    res.send(moveListAfterLastMove.split(" ")[2]);
     }
   }
 
 });
 app.get('/:code',(req,res)=>{
-  if(arrayOfThisRow[req.params.code]){
-    res.send(arrayOfThisRow[req.params.code]);
+  if(moveJson[req.params.code]){
+    res.send(moveJson[req.params.code]);
   }else{
     res.send('Not found');
   }
 });
-
-
 
 app.listen(port, () => {
   console.log('Server is running on port 3000');
